ba1885d9a7151899c9bea196da3ddc774b76982f
Kmillay <82171036+Kmillay@users.noreply.github.com>
2021-06-06 18:59:43 -0500

    http

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject } from 'rxjs'
 import { Recipe } from '../recipes/recipe.model'
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
@@ -29,6 +29,10 @@ export class RecipeService {
 
   constructor(private slService: ShoppingListService) {}
 
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
@@ -53,7 +57,7 @@ export class RecipeService {
   }
 
   deleteRecipe(index: number) {
-    this.recipes.splice(index, 1);
+    this.recipes.splice(index, 1)
     this.recipesChanged.next(this.recipes.slice());
   }
 
