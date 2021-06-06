6696bd5a2c6041ec45d0e39fc9dd134050cd7a88
Kmillay <82171036+Kmillay@users.noreply.github.com>
2021-06-06 18:59:43 -0500

    http

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component ({
   selector: 'app-header',
   templateUrl: '../header/header.component.html'
 })
 export class HeaderComponent {
+  constructor(private DataStorageService: DataStorageService) {}
 
+  onSaveData() {
+    this.DataStorageService.storeRecipes();
+  }
 
+  onFetchData() {
+    this.DataStorageService.fetchRecipes().subscribe();
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,32 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs'
 import { Recipe } from '../recipes/recipe.model'
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-    new Recipe(
-    'Test Recipe',
-    'This is a Test Recipe',
-    'https://get.pxhere.com/photo/food-cuisine-dish-ingredient-bread-baked-goods-soda-bread-produce-staple-food-stollen-hefekranz-loaf-damper-dessert-Easter-bread-finger-food-sourdough-rye-bread-recipe-gluten-potato-bread-1558091.jpg',
-     [
-       new Ingredient('Meat', 1),
-       new Ingredient('French Fries', 17)
-     ]),
-    new Recipe(
-    'Another Test Recipe',
-    'This is a Test Recipe',
-    'https://get.pxhere.com/photo/food-cuisine-dish-ingredient-bread-baked-goods-soda-bread-produce-staple-food-stollen-hefekranz-loaf-damper-dessert-Easter-bread-finger-food-sourdough-rye-bread-recipe-gluten-potato-bread-1558091.jpg',
-    [
-      new Ingredient('Bun', 2),
-      new Ingredient('Meat', 2)
-    ])
-  ];
+  //   private recipes: Recipe[] = [
+  //   new Recipe(
+  //   'Sandwich',
+  //   'Sandwich Test Recipe',
+  //   'https://get.pxhere.com/photo/food-cuisine-dish-ingredient-bread-baked-goods-soda-bread-produce-staple-food-stollen-hefekranz-loaf-damper-dessert-Easter-bread-finger-food-sourdough-rye-bread-recipe-gluten-potato-bread-1558091.jpg',
+  //    [
+  //      new Ingredient('Meat', 1),
+  //      new Ingredient('French Fries', 17)
+  //    ]),
+  //   new Recipe(
+  //   'Burger',
+  //   'Burger Test Recipe',
+  //   'https://get.pxhere.com/photo/food-cuisine-dish-ingredient-bread-baked-goods-soda-bread-produce-staple-food-stollen-hefekranz-loaf-damper-dessert-Easter-bread-finger-food-sourdough-rye-bread-recipe-gluten-potato-bread-1558091.jpg',
+  //   [
+  //     new Ingredient('Bun', 2),
+  //     new Ingredient('Meat', 2)
+  //   ])
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
 
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
@@ -38,6 +46,21 @@ export class RecipeService {
 
   addToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe){
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1)
+    this.recipesChanged.next(this.recipes.slice());
   }
 
 }
