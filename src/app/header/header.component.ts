74ac87fa3a31631711441f32b0843823fb1c2a29
Kmillay <82171036+Kmillay@users.noreply.github.com>
2021-06-13 12:56:00 -0500

    finished http

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component ({
   selector: 'app-header',
   templateUrl: '../header/header.component.html'
 })
 export class HeaderComponent {
+  constructor(private dataStorageService: DataStorageService) {}
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
+
+  onFetchData() {
+    this.dataStorageService.fetchRecipes().subscribe();
+  }
+  }
 
 
-}
